b5555ac8a87227d4303855e4565115b564140eca
Taylor J Saintable <taylor.j.saintable@gmail.com>
2024-05-23 17:36:50 -0400

    Formatting

--- BudgetWise/obj/Debug/net8.0/BudgetWise.AssemblyInfo.cs
+++ BudgetWise/obj/Debug/net8.0/BudgetWise.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BudgetWise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec8988eca4bc5077327d26d2be7a56db2531fa60")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89bd5dd3b70d1b95764ef28e07cbf3d2150a28eb")]
 [assembly: System.Reflection.AssemblyProductAttribute("BudgetWise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BudgetWise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
